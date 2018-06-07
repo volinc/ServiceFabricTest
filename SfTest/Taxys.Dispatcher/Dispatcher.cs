124fa837d0cb242d9e5b71ab85e933ff99214fda
vol.inc <vol.inc@taxys.ru>
2018-06-07 18:39:07 +0300

    ... second init

---
+++ SfTest/Taxys.Dispatcher/Dispatcher.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+
+namespace Taxys.Dispatcher
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class Dispatcher : StatefulService
+    {
+        public Dispatcher(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new ServiceReplicaListener[]
+            {
+                new ServiceReplicaListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton(serviceContext)
+                                            .AddSingleton(this.StateManager))
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.UseUniqueServiceUrl)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+}
---
+++ SfTest/Taxys.Gate/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Taxys.Gate.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public ValuesController(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {          
+            var cancellationToken = CancellationToken.None;            
+            var resolver = ServicePartitionResolver.GetDefault();
+            var serviceUri = new Uri("fabric:/SfTest/Taxys.Auth");
+            var partition = resolver.ResolveAsync(serviceUri, new ServicePartitionKey(), cancellationToken).Result;    
+            var endpoint = partition.GetEndpoint();
+                
+            var addresses = JObject.Parse(endpoint.Address);
+            var address = (string)addresses["Endpoints"].First();
+
+            var httpResponse = httpClient.GetAsync($"{address}/api/values").Result;
+            
+            var content = httpResponse.Content.ReadAsAsync<IEnumerable<string>>().Result;
+            return content;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
